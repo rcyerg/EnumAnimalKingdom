f506238fefe35f63ce8f372d5593b8e67a7c7c76
rcyerg <rcyerger02@gmail.com>
2024-04-05 15:55:40 -0400

    added robodog

---
+++ src/main/java/animalkingdom/RoboDog.java
@@ -0,0 +1,8 @@
+package animalkingdom;
+
+public class RoboDog extends Robot implements Pet {
+    @Override
+    public void greetOwner() {
+        System.out.println("rolls on wheel and lights up");
+    }
+}
